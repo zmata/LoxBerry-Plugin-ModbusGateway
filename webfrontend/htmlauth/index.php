0ed3cab40e76f4b8d75e5f03851fad4fc142200f
zmata <37386532+zmata@users.noreply.github.com>
2018-03-28 13:31:26 +0200

    Update index.php

--- webfrontend/htmlauth/index.php
+++ webfrontend/htmlauth/index.php
@@ -1,8 +1,35 @@
-<html>
- <head>
-  <title>PHP-Test</title>
- </head>
- <body>
- <?php echo '<p>Hallo Welt</p>'; ?>
- </body>
-</html>
+<?php
+
+require_once "loxberry_system.php";
+require_once "loxberry_web.php";
+
+$L = LBWeb::readlanguage("language.ini");
+
+$template_title = "Modbus Gateway";
+$helplink = "http://www.loxwiki.eu:80/x/_wFmAQ";
+$helptemplate = "pluginhelp.html";
+
+$navbar[1]['Name'] = $L['NAVBAR.FIRST'];
+$navbar[1]['URL'] = 'index.php';
+
+$navbar[2]['Name'] = $L['NAVBAR.SECOND'];
+$navbar[2]['URL'] = 'settings.php';
+
+
+// Activate the first element
+$navbar[1]['active'] = True;
+
+LBWeb::lbheader($template_title, $helplink, $helptemplate);
+
+?>
+<p><?=$L['MAIN.INTRO']?></p>
+<p><?=$L['MAIN.USAGE1']?></p>
+<p><?=$L['MAIN.USAGE2']?></p>
+<p></p>
+
+
+
+
+<?php
+LBWeb::lbfooter();
+?>
