bf2773f7a2cdd03f1418ff07da69049e2c0ef859
zmata <37386532+zmata@users.noreply.github.com>
2018-03-31 02:19:00 +0200

    Update log.php

--- webfrontend/htmlauth/log.php
+++ webfrontend/htmlauth/log.php
@@ -28,7 +28,7 @@ if ($handle = opendir($lbplogdir)) {
     while (false !== ($entry = readdir($handle))) {
         if ($entry != "." && $entry != "..") {
           echo '<div class="ui-corner-all ui-shadow">';
-          echo '<a id="btnlogs" data-role="button" href="/admin/system/tools/logfile.cgi?logfile='. $lbplogdir. '/'. $entry. '&header=html&format=template" target="_blank" data-inline="true" data-mini="true">'.$entry. '</a>';
+          echo '<a id="btnlogs" data-role="button" href="/admin/system/tools/logfile.cgi?logfile=plugins/modbusgateway/'. $entry. '&header=html&format=template" target="_blank" data-inline="true" data-mini="true">'.$entry. '</a>';
           echo '</div>';
         }
     }
